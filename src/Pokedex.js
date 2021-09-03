2410c2fecc3ae0742d25432abdc87c3ff3857aca
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-09-02 21:14:00 -0300

    Exercício 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,30 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            counter: 0,
+        }
+
+        this.showNextPokemon = this.showNextPokemon.bind(this)
+    }
+
+    showNextPokemon() {
+        this.setState((prevState, props) => (prevState.counter < props.pokemons.length - 1 ? 
+            { counter: prevState.counter + 1 } : 
+            { counter: 0 }
+        ))
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                {this.props.pokemons
+                    .filter((_pokemon, index) => this.state.counter === index )
+                    .map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <button className="btn-next" onClick={this.showNextPokemon}>Next Pokemon</button>
             </div>
         );
     }
