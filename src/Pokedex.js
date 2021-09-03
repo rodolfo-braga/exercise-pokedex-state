56b4838acaa0ffe31176010f24dbf659bd2e0b61
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-09-02 21:56:43 -0300

    Exercício 2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -7,25 +7,39 @@ class Pokedex extends React.Component {
 
         this.state = {
             counter: 0,
+            type: 'Fire',
         }
 
         this.showNextPokemon = this.showNextPokemon.bind(this)
+        this.filterByType = this.filterByType.bind(this)
     }
 
     showNextPokemon() {
-        this.setState((prevState, props) => (prevState.counter < props.pokemons.length - 1 ? 
+        this.setState((prevState, props) => (prevState.counter < props.pokemons
+                .filter((pokemon) => pokemon.type === this.state.type).length - 1 ? 
             { counter: prevState.counter + 1 } : 
             { counter: 0 }
         ))
+    }
+
+    filterByType(event) {
+        this.setState({
+            type: event.target.value,
+        })
     }
 
     render() {
         return (
             <div className="pokedex">
                 {this.props.pokemons
+                    .filter((pokemon) => pokemon.type === this.state.type)
                     .filter((_pokemon, index) => this.state.counter === index )
                     .map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-                <button className="btn-next" onClick={this.showNextPokemon}>Next Pokemon</button>
+                <section className="filters-container">
+                    <button className="btn filter" value="Fire" onClick={this.filterByType}>Fire</button>
+                    <button className="btn filter" value="Psychic" onClick={this.filterByType}>Psychic</button>
+                </section>
+                <button className="btn next" onClick={this.showNextPokemon}>Next Pokemon</button>
             </div>
         );
     }
